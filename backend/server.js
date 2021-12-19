830379a43c609d2b259c67eaec32dac9bd62c078
fatma2201 <96368816+fatma2201@users.noreply.github.com>
2021-12-19 23:53:14 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,40 @@ const app=express()
 
 const db=require('./db')
 const Todo=require('./todo')
-console.log(Todo)
+//console.log(Todo)
+app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res)=>{
 res.json('Get / is working')
 })
-app.get('/tasks',(req,res)=>{
-    res.json('Get / is working')
-    })
+app.get("/tasks",(req,res)=>{
+    Todo.find({},(err,data)=>{
+        if(err) {
+         console.log('ERROR: ',err);
+
+        } else {
+            res.json('Get / is working');
+        } 
+    });
+   
+    });
+    app.post("/tasks",(req,res)=>{
+        console.log('25:',req.body)
+       // Todo.create({},(err,newTask)=>{
+            
+        //    if(err) {
+        //     console.log('ERROR: ',err);
+    
+        //    } else {
+                res.status(201).json(newTask)
+        //    }
+       // });
+       
+        });
+    
+
+    
 
 app.listen(5000,()=>{
-    console.log('Server is working..')
+    console.log('Server is working....')
 })
