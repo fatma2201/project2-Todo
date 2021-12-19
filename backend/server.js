3ee53a31b8579e6c28600cb8524669de669d7af2
fatma2201 <96368816+fatma2201@users.noreply.github.com>
2021-12-19 22:48:30 +0300

    Setup the project

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+
+const db=require('./db')
+const Todo=require('./todo')
+console.log(Todo)
+
+app.get('/',(req,res)=>{
+res.json('Get / is working')
+})
+app.get('/tasks',(req,res)=>{
+    res.json('Get / is working')
+    })
+
+app.listen(5000,()=>{
+    console.log('Server is working..')
+})
