0ef12141a74b49ade61a4aa88aaf891f1134522d
fatma2201 <96368816+fatma2201@users.noreply.github.com>
2021-12-22 02:40:58 +0300

    finish todo

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express=require('express')
+const cors=require('cors')
 const app=express()
 
 const db=require('./db')
 const Todo=require('./todo')
 //console.log(Todo)
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res)=>{
 res.json('Get / is working')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import './'
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import Todo from './components/Todo'
 export default function App() {
+  const [tasks,setTasks]=useState([])
   const getData=()=>{
-
+axios
+.get('http://localhost:5000/tasks')
+.then((response)=>{
+ // console.log('RESPONSE: ',response)
+  console.log('DATA: ', response.data)
+  setTasks(response.data)
+})
+.catch((err)=>{
+  console.log('ERR:',err)
+})
   }
+  const mapOverTasks=tasks.map((taskObj,i)=> (<Todo key={i}  task={taskObj}/> ))
+  
   return (
     <div className='app'>
       <p>app</p>
       <button onClick={getData}>Get TASKS</button>
+      {mapOverTasks}
     </div>
   )
 }
