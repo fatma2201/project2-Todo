25d8cd025617f965a67eea60341f3b7914a0b483
fatma2201 <96368816+fatma2201@users.noreply.github.com>
2021-12-22 01:05:09 +0300

    react started code

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './'
+export default function App() {
+  const getData=()=>{
+
+  }
+  return (
+    <div className='app'>
+      <p>app</p>
+      <button onClick={getData}>Get TASKS</button>
+    </div>
+  )
+}
+
